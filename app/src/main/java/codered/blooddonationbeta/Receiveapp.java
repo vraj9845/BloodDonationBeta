07db29ba33572e8924a91ab753d0f816861ddee6
vraj9845 <vraj9845@gmail.com>
2017-09-22 09:38:18 +0530

    HI 1st commit messahe bleh!

---
+++ app/src/main/java/codered/blooddonationbeta/Receiveapp.java
@@ -0,0 +1,16 @@
+package codered.blooddonationbeta;
+
+import android.app.Application;
+
+/**
+ * Created by vraj9 on 09/20/2017.
+ */
+
+public class Receiveapp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+
+    }
+}
